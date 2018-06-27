a873687b318983711e54d7ad6aa5ff96f864659f
Ivan <ikononov94@gmail.com>
2018-06-27 15:44:16 +0300

    Added redux provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMuddleware from 'redux-thunk';
+import reducer from './reducers';
+
 import './index.css';
-import App from './App';
+import Users from './containers/Users';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunkMuddleware),
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Users />
+    </Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
