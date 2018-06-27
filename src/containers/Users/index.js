e36acfbb656d63fb38868bde8c9898a26f47cbd4
Ivan <ikononov94@gmail.com>
2018-06-27 17:45:11 +0300

    Added slider into Users.js

--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import User from '../../components/User';
 import { fetchUsers } from '../../actions/users';
 import './style.css';
 
 class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    this.container = React.createRef();
+    this.prevUsers = this.prevUsers.bind(this);
+    this.nextUsers = this.nextUsers.bind(this);
+
+  }
   componentDidMount() {
     this.props.fetchUsers();
   }
 
+  prevUsers() {
+    this.container.current.scrollLeft -= 100;
+  }
+
+  nextUsers() {
+    this.container.current.scrollLeft += 100;
+  }
+
   render() {
-    console.log(this.props)
+    const { usersById, usersAllIds } = this.props;
+
     return (
-      <div></div>
+      <div className="slider">
+       <div className="slider-previous" onClick={this.prevUsers}></div>
+        <div className="slider-users" ref={this.container}>
+          { 
+            usersAllIds.map((userId) => (
+              <User userInfo={usersById[userId]} key={userId} />
+            ))
+          }
+        </div>
+        <div className="slider-next" onClick={this.nextUsers}></div>
+      </div>
     );
   }
 }
