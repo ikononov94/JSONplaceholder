2d291ba4e6ed93d5cd942a56f3a6feb8fda252fe
Ivan <ikononov94@gmail.com>
2018-06-28 16:56:49 +0300

    Add reducer post in CombineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import users from './users';
 import posts from './posts';
+import post from './post';
 import comments from './comments';
 
 export default combineReducers({
     users,
     posts,
+    post,
     comments,
 });
