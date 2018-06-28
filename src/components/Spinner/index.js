540fb10efd7ac4165a7a3cd20776aaad8a86e112
Ivan <ikononov94@gmail.com>
2018-06-28 10:58:22 +0300

    Added component Spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const Spinner = () => (
+    <div className="spinner-wrapper">
+        <div className="spinner"></div>
+    </div>
+)
+
+export default Spinner;
